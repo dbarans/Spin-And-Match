bab0d471075e38059beac5a11b419a56a8b6f4cc
Dominik Baranski <baranskidominik109@gmail.com>
2024-03-04 23:54:00 +0100

    small code fix

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -4,14 +4,15 @@ using UnityEngine;
 
 public class Spawner : MonoBehaviour
 {
+    [SerializeField]
     private GameObject obstaclePrefab;
 
     void Start()
     {
-        obstaclePrefab = GameObject.FindGameObjectWithTag("Obstacle");
+        SpawnObstacle();
     }
 
-    public void SpawnObstacle()
+    void SpawnObstacle()
     {
         Instantiate(obstaclePrefab, transform.position, Quaternion.identity);
     }
